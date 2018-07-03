ac5d454d4cfd355150f1720197c4d7dc20751127
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-07-02 16:16:44 -1000

    Solution to TheVault assignment.

--- vault.js
+++ vault.js
@@ -1,4 +1,38 @@
 'use strict';
 module.exports = function() {
+  var val = {};
   
+  return {
+      setValue: setValue,
+      getValue: getValue
+  }
+
+  function setValue(key, value){
+    console.log('value: '+value+' key: '+key+'\n');
+    if (key !== null) {
+        val[key] = value;
+        console.log(val);
+    } else {
+        return null;
+    }
+  }
+
+  function getValue(key){
+
+      if(Object.keys(val).length > 0){
+        var keys = Object.keys(val);
+        
+        for (var i = 0; i < keys.length; i++) {
+
+            if (keys[i] === key) {
+                return val[keys[i]];
+            }
+
+        }
+
+      } else {
+        return null;
+      }
+  }
+
 };
